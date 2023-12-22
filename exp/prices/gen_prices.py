591c2c0e2bf1322c5ed8d608211609eaf5b1bf04
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2023-12-22 01:09:31 -0500

    update

--- exp/prices/gen_prices.py
+++ exp/prices/gen_prices.py
@@ -14,8 +14,15 @@ prices = [random.randint(1, 2000) for _ in range(num_lines)]
 # Write to CSV file
 with open('prices.csv', 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['rownumber', 'month', 'price'])  # Write header
+    csvwriter.writerow(['month', 'price'])  # Write header
     for rownumber, (month, price) in enumerate(zip(months, prices), start=1):
-        csvwriter.writerow([rownumber, month, price])
+        csvwriter.writerow([month, price])
+
+# // for avoiding clustered storage
+# with open('prices.csv', 'w', newline='') as csvfile:
+#     csvwriter = csv.writer(csvfile)
+#     csvwriter.writerow(['rownumber', 'month', 'price'])  # Write header
+#     for rownumber, (month, price) in enumerate(zip(months, prices), start=1):
+#         csvwriter.writerow([rownumber, month, price])
 
 print(f"CSV file with {num_lines} lines generated as 'prices.csv'")
